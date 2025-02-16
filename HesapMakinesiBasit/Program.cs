4265187c8678656abb31aa15968913c1e45f75f1
Gorkem Dikbakan <gorkem_gd@hotmail.com>
2025-02-16 15:31:07 +0300

    AcunMedya Hesap Makinesi Odevi

---
+++ HesapMakinesiBasit/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Bir İslem Giriniz (1- Toplama, 2- Cıkarma, 3- Çarpma, 4- Bölme :");
+int islem = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("birinci Sayıyı Giriniz : ");
+int sayi1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("İkinci Sayıyı Giriniz : ");
+int sayi2 = Convert.ToInt32(Console.ReadLine());
+switch (islem)
+{
+    case 1:
+        Console.WriteLine(sayi1 + sayi2);
+        break;
+    case 2:
+        Console.WriteLine(sayi1 - sayi2);
+        break;
+    case 3:
+        Console.WriteLine(sayi1 * sayi2);
+        break;
+    case 4:
+        Console.WriteLine(sayi1 / sayi2);
+        break;
+    default:
+        Console.WriteLine("Yanlış Değer Giriniz");
+        break;
+}
